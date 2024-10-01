645659a001775215375fbe017e9296aa89c925e5
GRoxo-B <gui.basso@gmail.com>
2024-09-30 22:10:47 -0300

    import fix

--- test/test.py
+++ test/test.py
@@ -1,9 +1,5 @@
 import unittest
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-from app import app
+from src.app import app
 
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
